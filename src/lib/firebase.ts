4b6a80129b3a4a81b6a435111d711c19a1ebf413
Jörg Holthöfer <holthofer@gmail.com>
2025-08-27 16:57:00 +0000

    meine Konfiguration zeigt keep-know.firebasestorage.app, wie kommst du a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   "projectId": "keep-know",
   "appId": "1:897948696245:web:993f704158bf4bd63e88fc",
-  "storageBucket": "keep-know.appspot.com",
+  "storageBucket": "keep-know.firebasestorage.app",
   "apiKey": "AIzaSyA7fATeewT2caPFET9uyd1YUEgkVvsRM8Q",
   "authDomain": "keep-know.firebaseapp.com",
   "measurementId": "",
