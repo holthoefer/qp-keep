8156ff602eaa401e19518da9fd91cace1e462128
Jörg Holthöfer <holthofer@gmail.com>
2025-08-31 20:01:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/auftraege/page.tsx
+++ src/app/auftraege/page.tsx
@@ -36,6 +36,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
